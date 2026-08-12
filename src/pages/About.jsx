0b3f9204fdb56087980cde550fb5790171421d77
Husayin Bahramov <husayinbahramov@gmial.com>
2026-08-12 19:28:00 +0500

    all is true

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
+import developer from "../assets/developer.jpg";
 
 const About = () => {
   return (
@@ -20,11 +21,27 @@ const About = () => {
       {/* menu text */}
       <div>
         {/* funder */}
-        <div></div>
+        <div className=" flex justify-center items-center flex-col mt-3.5 rounded-2xl bg-white w-[70%] py-3.75 m-auto ">
+          <img className=" h-60 rounded-2xl " src={developer} alt="" />
+          <h3 className=" font-bold">
+            <span className=" font-normal text-[12px] ">Developer:</span> Xusan
+            Baxramov
+          </h3>
+        </div>
         {/* investor */}
         <div></div>
         {/* about us */}
-        <div></div>
+        <div className=" flex justify-center items-center flex-col text-center mt-3.5 px-3.5">
+          <h2 className=" mb-1 font-bold ">AvtoTek - loyiha maqsadi</h2>
+          <p>
+            **AvtoTek — ishonchli va shaffof avto-bozor!** Bizning asosiy
+            maqsadimiz — har bir avtomobilni sotuvga qo'yishdan oldin chuqur
+            texnik va huquqiy tekshiruvdan o'tkazishdir. Mijozlarimizga sifatli
+            avtolarni taqdim etish bilan birga, halol nasiya savdo tizimini ham
+            taklif qilamiz. AvtoTek bilan xaridingiz halol, xavfsiz va mutlaqo
+            ishonchli bo'ladi!
+          </p>
+        </div>
       </div>
     </div>
   );
